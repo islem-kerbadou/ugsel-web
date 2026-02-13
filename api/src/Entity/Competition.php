61a0f49c282239bed06ae66a2c9de4953f01388e
Flavio <flavio.oderzo@etu.univ-amu.fr>
2026-02-13 10:29:19 +0100

    chore: line too long

--- api/src/Entity/Competition.php
+++ api/src/Entity/Competition.php
@@ -27,7 +27,12 @@ class Competition
     /**
      * @var Collection<int, Championship>
      */
-    #[ORM\OneToMany(targetEntity: Championship::class, mappedBy: "competition", cascade: ['remove'], orphanRemoval: true)]
+    #[ORM\OneToMany(
+        targetEntity: Championship::class,
+        mappedBy: "competition",
+        cascade: ['remove'],
+        orphanRemoval: true
+    )]
     private Collection $championships;
 
     public function __construct()
