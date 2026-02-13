873958c809f5a72f7aa39aa5b192a9a8513ee109
Flavio <flavio.oderzo@etu.univ-amu.fr>
2026-02-13 10:51:48 +0100

    chore: wrong file names

--- api/tests/SportTypeApiTest.php
+++ api/tests/SportTypeApiTest.php
@@ -4,7 +4,7 @@ namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class SportApiTest extends WebTestCase
+class SportTypeApiTest extends WebTestCase
 {
     protected static function getKernelClass(): string
     {
